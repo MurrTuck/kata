6c19d43d0b30ca8662a097092e37d35c31960e4c
MurrTuck <murrtuck@gmail.com>
2020-06-16 17:11:08 -0600

    Opposite number

---
+++ src/oppositeNumber.js
@@ -0,0 +1,9 @@
+// Very simple, given a number, find its opposite.
+
+// https://www.codewars.com/kata/56dec885c54a926dcd001095/train/javascript
+
+opposite = (number) => {
+    return number * -1
+}
+
+console.log(opposite(27))
