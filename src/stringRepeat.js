2c36827e3813593ef158d39280dc2b2ca7c25b69
MurrTuck <murrtuck@gmail.com>
2020-07-08 15:07:25 -0600

    Getting to GitHub

--- src/stringRepeat.js
+++ src/stringRepeat.js
@@ -2,11 +2,26 @@
 
 // https://www.codewars.com/kata/57a0e5c372292dd76d000d7e/train/javascript
 
-function repeatStr(n, s) {
+repeatStr = (n, s) => {
     var str = s
     return str.repeat(n);
 }
 
-const newString = repeatStr(17, 'ForestGump')
+const newString = repeatStr(17, 'Forest Gump ')
 
 console.log(newString)
+
+
+// classmates code
+
+// function repeatStr2(num, str) {
+//     let string = str
+//     for (let i = num; i > 1; i--) {
+//         string += str
+//     }
+//     return string
+// }
+
+// const newString2 = repeatStr2(4, 'Pig')
+
+// console.log(newString2)
